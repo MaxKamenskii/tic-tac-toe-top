d9ae0561120ba231edc521dd279bfedf96d0b546
Max Kamenskii <80255339+MaxKamenskii@users.noreply.github.com>
2025-05-29 09:58:33 +0500

    Check the winner

--- script.js
+++ script.js
@@ -21,7 +21,71 @@ function Gameboard() {
         }
 
     const getCell = (row, column) => board[row][column].getValue()
-    return {getBoard, printBoard, markBoard, getCell}
+
+    const checkWinner = (playerName) => {
+        let firstRow = [board[0][0].getValue(), board[0][1].getValue(), board[0][2].getValue()];
+        let secondRow = [board[1][0].getValue(), board[1][1].getValue(), board[1][2].getValue()];
+        let thirdRow = [board[2][0].getValue(), board[2][1].getValue(), board[2][2].getValue()];
+        let rightDiagonal = [board[2][0].getValue(), board[1][1].getValue(), board[0][2].getValue()];
+        let leftDiagonal = [board[0][0].getValue(), board[1][1].getValue(), board[2][2].getValue()];
+        let firstColumn = [board[0][0].getValue(), board[1][0].getValue(), board[2][0].getValue()];
+        let secondColumn = [board[0][1].getValue(), board[1][1].getValue(), board[2][1].getValue()];
+        let thirdColumn = [board[0][2].getValue(), board[1][2].getValue(), board[2][2].getValue()];
+        // Player 1 conditions
+        if(firstRow.toString() === "1,1,1") {
+            console.log(`${playerName} wins  by marking the first row`)
+            return true;
+        } else if(secondRow.toString() === "1,1,1") {
+            console.log(`${playerName} wins  by marking the second row`)
+            return true;
+        } else if(thirdRow.toString() === "1,1,1") {
+            console.log(`${playerName} wins  by marking the third row`)
+            return true;
+        } else if(rightDiagonal.toString() === "1,1,1") {
+            console.log(`${playerName} wins  by marking the right diagonal`)
+            return true;
+        } else if(leftDiagonal.toString() === "1,1,1") {
+            console.log(`${playerName} wins  by marking the left diagonal`)
+            return true;
+        } else if(firstColumn.toString() === "1,1,1") {
+            console.log(`${playerName} wins  by marking the first column`)
+            return true;
+        } else if(secondRow.toString() === "1,1,1") {
+            console.log(`${playerName} wins  by marking the second column`)
+            return true;
+        } else if(thirdColumn.toString() === "1,1,1") {
+            console.log(`${playerName} wins  by marking the third column`)
+            return true;
+        }
+        // Player 2 conditions
+        else if(firstRow.toString() === "2,2,2") {
+            console.log(`${playerName} wins  by marking the first row`)
+            return true;
+        } else if(secondRow.toString() === "2,2,2") {
+            console.log(`${playerName} wins  by marking the second row`)
+            return true;
+        } else if(thirdRow.toString() === "2,2,2") {
+            console.log(`${playerName} wins  by marking the third row`)
+            return true;
+        } else if(rightDiagonal.toString() === "2,2,2") {
+            console.log(`${playerName} wins  by marking the right diagonal`)
+            return true;
+        } else if(leftDiagonal.toString() === "2,2,2") {
+            console.log(`${playerName} wins  by marking the left diagonal`)
+            return true;
+        } else if(firstColumn.toString() === "2,2,2") {
+            console.log(`${playerName} wins  by marking the first column`)
+            return true;
+        } else if(secondRow.toString() === "2,2,2") {
+            console.log(`${playerName} wins  by marking the second column`)
+            return true;
+        } else if(thirdColumn.toString() === "2,2,2") {
+            console.log(`${playerName} wins  by marking the third column`)
+            return true;
+        }
+    }
+
+    return {getBoard, printBoard, markBoard, getCell, checkWinner}
 }
 
 
@@ -41,6 +105,12 @@ function Cell() {
         getValue
     };
 }
+
+// function checkWinner () {
+//     // const gameboard = Gameboard()
+
+//     const board =  
+// }
 
 function GameController(playerOneName = 'Player One',
     playerTwoName = 'Player Two') {
@@ -72,16 +142,23 @@ function GameController(playerOneName = 'Player One',
         console.log(`${getActivePlayer().name}'s turn`)
     }
 
+
     const playRound = (row, column) => {
+        
         theCell = board.getCell(row, column)
         console.log(`Get cell is: ${theCell}`)
         if(theCell === 0) {
             console.log(`${getActivePlayer().name} marks raw ${row} and column ${column}...`)
             board.markBoard(row, column, getActivePlayer().mark)
+                if(board.checkWinner(activePlayer.name)){
+                    console.log("Game over");
+                    return;
+                } else {
+                    // Switch player turn
+                    switchPlayerTurn();
+                    printNewRound(); 
+                }
 
-                // Switch player turn
-                switchPlayerTurn();
-                printNewRound();
         } else {
             console.log('The cell is occupied by another player, try again')
             printNewRound();
